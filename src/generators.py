10fc74820a9a21a814175a5ca51368f7e74830a7
Flow-zxc <fransandchans@gmail.com>
2025-05-06 18:28:49 +0300

    nothing

--- src/generators.py
+++ src/generators.py
@@ -1,22 +1,27 @@
-import random
-from typing import Any, Generator, Iterator, List
+def filter_by_currency(transactions, currency):
+    """Возвращает итератор с транзакциями, где валюта соответствует заданной."""
+    return filter(
+        lambda transaction: transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency
+        or transaction.get("currency_code") == currency,
+        transactions,
+    )
 
 
-def filter_by_currency(transactions: List[dict], currency: str = "USD") -> Iterator[dict]:
-    """Функция,принимает список словарей и возвращает операции, в которых указана валюта"""
-    for transaction in transactions:
-        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
-            yield transaction
+def transaction_descriptions(transactions):
+    """Генерирует описания транзакций."""
+    for tran in transactions:
+        if tran:  # Проверка на пустой словарь
+            yield tran.get("description")
 
 
-def transaction_descriptions(transactions: List[dict]) -> Generator[str, None, None]:
-    """Генератор, принимает список словарей и возращает по очереди в нужном формате"""
-    for transaction in transactions:
-        yield str(transaction.get("description"))
+def card_number_generator(start, last):
+    """Генерирует номера карт в заданном диапазоне."""
 
+    def format_card_number(number):
+        """Форматирует номер карты в стандартный формат XXXX XXXX XXXX XXXX."""
+        string = f"{number:016}"
+        return " ".join([string[i : i + 4] for i in range(0, len(string), 4)])
 
-def card_number_generator(start: int, end: int) -> Generator[str, Any, None]:
-    """Генератор номеров банковских карт в нужном формате"""
-    for _ in range(start, end + 1):
-        card_number = "".join([str(random.randint(0, 9)) for _ in range(16)])
-        yield " ".join([card_number[i : i + 4] for i in range(0, len(card_number), 4)])
+    while start <= last:
+        yield format_card_number(start)
+        start += 1
